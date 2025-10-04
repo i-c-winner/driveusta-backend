c7178bd4010f348f7253ae649bf5d1e7a390dbc7
Belousov Dmitriy <d-belousov@hotmail.com>
2025-10-04 20:20:08 +0500

    feat: изменит стартовую страницу

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+
 from .api import router
 
 app = FastAPI()
 
 app.include_router(router, prefix="/api")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def root():
-    return {"message": "Hello from backend"}
+    return """
+        <html>
+            <head>
+                <title>Hello</title>
+            </head>
+            <body>
+                <p>Привет</p>
+                <p>Описание Api можно посмотреть на этом же host добавив '/docs'</p>
+                <p>Сам репозиторий можно найти https://github.com/i-c-winner/driveusta-backend</p>
+            </body>
+        </html>
+        """
 
